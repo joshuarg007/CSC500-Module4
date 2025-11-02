1edc9b3888a0c55ea6afc88a803be3a47f50b18d
Joshuarg007 <joshuarg007@gmail.com>
2025-11-02 14:38:18 -0700

    Updated user input to use loop structure

--- online_shopping_cart.py
+++ online_shopping_cart.py
@@ -10,7 +10,7 @@ It defines an ItemToPurchase class and calculates the total cost of two items
 entered by the user.
 """
 
-# Step 1: Define ItemToPurchase class
+# Step 1: Define class
 class ItemToPurchase:
     def __init__(self, item_name="none", item_price=0.0, item_quantity=0):
         self.item_name = item_name
@@ -21,23 +21,24 @@ class ItemToPurchase:
         total_cost = self.item_price * self.item_quantity
         print(f"{self.item_name} {self.item_quantity} @ ${self.item_price:.0f} = ${total_cost:.0f}")
 
-# Step 2: Prompt the user for two items
-print("Item 1")
-item1_name = input("Enter the item name:\n")
-item1_price = float(input("Enter the item price:\n"))
-item1_quantity = int(input("Enter the item quantity:\n"))
-item1 = ItemToPurchase(item1_name, item1_price, item1_quantity)
+# Step 2: Prompt user for n items
+items = []  # List to store ItemToPurchase objects
 
-print("\nItem 2")
-item2_name = input("Enter the item name:\n")
-item2_price = float(input("Enter the item price:\n"))
-item2_quantity = int(input("Enter the item quantity:\n"))
-item2 = ItemToPurchase(item2_name, item2_price, item2_quantity)
+for i in range(1, 3): # loop to receive user input
+    print(f"Item {i}") # Header for prompt
+    name = input("Enter the item name:\n")
+    price = float(input("Enter the item price:\n"))
+    quantity = int(input("Enter the item quantity:\n"))
+    items.append(ItemToPurchase(name, price, quantity))
+    print()  
 
 # Step 3: Output the total cost
-print("\nTOTAL COST")
-item1.print_item_cost()
-item2.print_item_cost()
+print("TOTAL COST") # Header for output
+total_cost = 0 # initialize total cost
 
-total_cost = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
+for item in items: # Iterate over and add cost of each item added
+    item.print_item_cost()
+    total_cost += item.item_price * item.item_quantity
+
 print(f"\nTotal: ${total_cost:.0f}")
+
